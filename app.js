c6349c378b952e0c262d2fe4eca81d2c0cb9ca7f
Josue <josue.bills@gmail.com>
2022-01-25 00:16:24 -0800

    add solution code.

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = Vue.createApp({
+    data () {
+        return {
+            name: 'Josue Rivera',
+            age: 37,
+            randoImg: 'https://res.cloudinary.com/dfuxcsi7w/image/upload/v1643078987/bulmaPortfolio/PG_Natours.png'
+        }
+    }
+});
+
+app.mount('#assignment')
